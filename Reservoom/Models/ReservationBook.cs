e001b39b2f1f57779505531df1b1adc850108085
dacluan <luandk8688@gmail.com>
2023-08-24 17:08:57 +0700

    Add project files

---
+++ Reservoom/Models/ReservationBook.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservoom.Models
+{
+    public class ReservationBook
+    {
+        private readonly List<Reservation> _reservations;
+        public ReservationBook()
+        {
+            _reservations = new List<Reservation>();
+        }
+
+        public IEnumerable<Reservation> GetReservationsForUser(string user)
+        {
+            return _reservations.Where(r => r.Username == user);
+        }
+
+        public void AddReservation(Reservation reservation)
+        {
+            foreach (Reservation exstingRevervation in _reservations)
+            {
+                if(exstingRevervation.Conflicts(reservation))
+                {
+                    throw new ReservationConflictException();
+                }
+            }
+        }
+    }
+}
